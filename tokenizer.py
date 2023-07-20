0cf6fea58dd54aae0c2abad75a4f9bd185e36197
AzarUmudov <azarumudov@gmail.com>
2023-07-19 23:49:03 -0700

    A code snippet for loading tokenizer is added

---
+++ tokenizer.py
@@ -0,0 +1,37 @@
+import torch 
+import torch.nn as nn
+from pathlib import Path
+from tokenizers import Tokenizer 
+from tokenizers.models import WordPiece, BPE, WordLevel
+from tokenizers.trainers import WordPieceTrainer, BpeTrainer, WordLevelTrainer
+from tokenizers.pre_tokenizers import Whitespace
+
+unk_token = "[UNK]"
+special_tokens=["[UNK]", "[PAD]", "[SOS]", "[EOS]"]
+
+def load_sentences(dataset, split):
+    for data in dataset: 
+        yield data[split]
+
+def load_tokenizer(tokenizer_algo, dataset, split, config):
+    if tokenizer_algo == 'BPE':
+        tokenizer = Tokenizer(BPE(unk_token=unk_token))
+        trainer = BpeTrainer(special_tokens=special_tokens)
+    elif tokenizer_algo == 'WordPiece':
+        tokenizer = Tokenizer(WordPiece(unk_token=unk_token))
+        trainer = WordPieceTrainer(special_tokens=special_tokens)
+    elif tokenizer_algo == 'WordLevel':
+        tokenizer = Tokenizer(WordLevel(unk_token=unk_token))
+        trainer = WordLevelTrainer(special_tokens=special_tokens)
+    else:
+        raise Exception("Tokenizer algorithm is not found")
+    
+    tokenizer_path = Path(config['tokenizer_file'].format(split))
+    if not Path.exists(tokenizer_path):
+        tokenizer.pre_tokenizer = Whitespace()
+        tokenizer.train_from_iterator(iterator=load_sentences(dataset, split), trainer=trainer)
+        tokenizer.save()
+    else:
+        tokenizer = Tokenizer.from_file(str(tokenizer_path))
+    
+    return tokenizer
